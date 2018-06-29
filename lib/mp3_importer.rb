0a81f94b7143b72ee166bbd1e95487c0ff179206
Anusha Konatala <anu.konatala@gmail.com>
2018-06-29 03:19:43 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,31 @@
+require_relative "./song.rb"
+class MP3Importer
+attr_accessor :path
+@@filenames = []
+
+  def initialize(path)
+    @path = path
+    @@filenames = []
+    
+  end
+  
+  def files
+    allfiles = Dir.entries(@path)
+    allfiles.each do |file|
+       if file.end_with? '.mp3'
+         @@filenames << file
+       end
+     end
+    @@filenames
+  end
+
+ 
+
+  def import
+    @@filenames.each {|file| Song.new_by_filename(file)}
+  end
+  
+end
+
+
+
